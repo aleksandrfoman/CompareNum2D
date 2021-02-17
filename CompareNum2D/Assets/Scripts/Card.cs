74679c818409a472899f1f0add59965fdc2e67aa
aleksandrfoman <50654588+aleksandrfoman@users.noreply.github.com>
2021-02-17 15:51:12 +0200

    DropAndDragSystemHendler

---
+++ CompareNum2D/Assets/Scripts/Card.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Card : MonoBehaviour,IBeginDragHandler,IDragHandler,IEndDragHandler
+{
+    private Camera _mainCamera;
+    private Vector3 _offset;
+    public Transform defaultParent , defaultTempCardParent;
+    GameObject tempCardGO;
+
+    private void Awake()
+    {
+        _mainCamera = Camera.main;
+        tempCardGO = GameObject.Find("TempCardGO");
+    }
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        _offset = transform.position - _mainCamera.ScreenToWorldPoint(eventData.position);
+        defaultParent = defaultTempCardParent = transform.parent;
+
+        tempCardGO.transform.SetParent(defaultParent);
+        tempCardGO.transform.SetSiblingIndex(transform.GetSiblingIndex());
+
+        transform.SetParent(defaultParent.parent);
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Vector3 newPos = _mainCamera.ScreenToWorldPoint(eventData.position);
+        
+        transform.position = newPos +_offset;
+
+
+        if (tempCardGO.transform.parent != defaultTempCardParent)
+        {
+            tempCardGO.transform.SetParent(defaultTempCardParent); 
+        }
+        CheckPosition();
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        transform.SetParent(defaultParent);
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
+
+        transform.SetSiblingIndex(tempCardGO.transform.GetSiblingIndex());
+        tempCardGO.transform.SetParent(GameObject.Find("Canvas").transform);
+        tempCardGO.transform.localPosition = new Vector3(2150, 0);
+    }
+
+    private void CheckPosition()
+    {
+        int newIndex = defaultTempCardParent.childCount;
+        for (int i = 0; i < defaultTempCardParent.childCount; i++)
+        {
+            if(transform.position.x< defaultTempCardParent.GetChild(i).position.x)
+            {
+                newIndex = i;
+                if (tempCardGO.transform.GetSiblingIndex() < newIndex)
+                {
+                    newIndex--;
+                }
+                break;
+            }
+        }
+
+        tempCardGO.transform.SetSiblingIndex(newIndex);
+    }
+}
---
+++ CompareNum2D/Assets/Scripts/DropPlace.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DropPlace : MonoBehaviour,IDropHandler,IPointerEnterHandler,IPointerExitHandler
+{
+
+    
+    public void OnDrop(PointerEventData eventData)
+    {
+        Card card = eventData.pointerDrag.GetComponent<Card>();
+        if (card)
+        {
+            card.defaultParent = transform;
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (eventData.pointerDrag == null)
+        {
+            return;
+        }
+        Card card = eventData.pointerDrag.GetComponent<Card>();
+        if (card)
+        {
+            card.defaultTempCardParent = transform;
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (eventData.pointerDrag == null)
+        {
+            return;
+        }
+        Card card = eventData.pointerDrag.GetComponent<Card>();
+        if (card&&card.defaultTempCardParent == transform)
+        {
+            card.defaultTempCardParent = card.defaultParent;
+        }
+
+    }
+}
